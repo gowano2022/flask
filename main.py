fad3528d8476dcca38848025b7b48a4083f1f86a
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 14:49:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, jsonify
 import os
+from telegram import Bot
 
 app = Flask(__name__)
 
 
+
+# Set up your Telegram bot token
+bot_token = 'your_bot_token_here'
+
+# Create a bot instance
+bot = Bot(token=bot_token)
+
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def send_message():
+    # Send a message to the chat
+    chat_id = 'your_chat_id_here'
+    bot.send_message(chat_id=chat_id, text='Hello, world!')
+    return 'Message sent!'
 
 
 if __name__ == '__main__':
