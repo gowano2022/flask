96c5dd18a961d222e9e7a5d4816df525051e1d20
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-24 14:50:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 # Set up your Telegram bot token
-bot_token = 'your_bot_token_here'
+bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
 
 # Create a bot instance
 bot = Bot(token=bot_token)
@@ -15,7 +15,7 @@ bot = Bot(token=bot_token)
 @app.route('/')
 def send_message():
     # Send a message to the chat
-    chat_id = 'your_chat_id_here'
+    chat_id = 'localipy'
     bot.send_message(chat_id=chat_id, text='Hello, world!')
     return 'Message sent!'
 
